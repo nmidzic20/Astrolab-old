06dfcba0e799794be40baa77ca643377c410f365
Noa Midzic <nmidzic@yahoo.com>
2023-03-05 18:36:09 +0100

    Implemented NavigationDrawer functionality - populated navDrawerView manu dynamically with mainPagerAdapter's fragments list - when button pressed, it goes to that fragment

--- Astrolab/app/src/main/java/endava/project/starstruck/MainActivity.kt
+++ Astrolab/app/src/main/java/endava/project/starstruck/MainActivity.kt
@@ -2,8 +2,10 @@ package endava.project.starstruck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import endava.project.starstruck.adapters.MainPagerAdapter
@@ -18,21 +20,25 @@ class MainActivity : AppCompatActivity() {
     lateinit var tabLayout: TabLayout
     lateinit var viewPager2: ViewPager2
 
+    lateinit var navDrawerLayout: DrawerLayout
+    lateinit var navDrawerView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        setFragments()
+        val mainPagerAdapter = MainPagerAdapter(supportFragmentManager, lifecycle)
+
+        setFragments(mainPagerAdapter)
+        setNavigationDrawer(mainPagerAdapter)
     }
 
-    private fun setFragments()
+    private fun setFragments(mainPagerAdapter : MainPagerAdapter)
     {
         tabLayout = binding.tabs
         viewPager2 = binding.viewpager
-
-        val mainPagerAdapter = MainPagerAdapter(supportFragmentManager, lifecycle)
 
         mainPagerAdapter.addFragment(
             MainPagerAdapter.FragmentItem(
@@ -54,5 +60,31 @@ class MainActivity : AppCompatActivity() {
             tab.setText(mainPagerAdapter.fragmentItems[position].titleRes)
             tab.setIcon(mainPagerAdapter.fragmentItems[position].iconRes)
         }.attach()
+    }
+
+    private fun setNavigationDrawer(mainPagerAdapter : MainPagerAdapter)
+    {
+        navDrawerLayout = binding.navDrawerLayout
+        navDrawerView = binding.navView
+
+        mainPagerAdapter.fragmentItems.withIndex().forEach { (index, fragmentItem) ->
+            navDrawerView.menu
+                .add(fragmentItem.titleRes)
+                .setIcon(fragmentItem.iconRes)
+                .setCheckable(true)
+                .setChecked((index == 0))
+                .setOnMenuItemClickListener {
+                    viewPager2.setCurrentItem(index, true)
+                    navDrawerLayout.closeDrawers()
+                    return@setOnMenuItemClickListener true
+                }
+        }
+
+        //update checked item in nav menu as soon as page is changed
+        viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                navDrawerView.menu.getItem(position).isChecked = true
+            }
+        })
     }
 }
